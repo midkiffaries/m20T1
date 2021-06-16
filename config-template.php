df53ec5cf56934cad6115f03d00a83ee76ca6edc
Ted Balmer <Ted.balmer@gmail.com>
2021-06-15 21:44:59 -0500

    06-15-2021

--- config-template.php
+++ config-template.php
@@ -15,4 +15,11 @@ $config = json_decode($ThemeSettings);
 //$ThemeSettings = file_get_contents('config.json');
 //$config = json_decode($ThemeSettings);
 
+/*
+WordPress plugins used in this theme:
+Breadcrumb Trail - https://wordpress.org/plugins/breadcrumb-trail/
+Quick Featured Images - https://wordpress.org/plugins/quick-featured-images/
+XML Sitemaps - https://wordpress.org/plugins/google-sitemap-generator/
+*/
+
 ?>

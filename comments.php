b7b83df29fc616ff97636db834c4e69ffcbec3a7
Ted Balmer <Ted.balmer@gmail.com>
2021-06-20 12:36:08 -0500

    06-20-2021

--- comments.php
+++ comments.php
@@ -3,7 +3,7 @@
 <?php
 // Do not delete these lines
 if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-	die ('Please do not load this page directly. Thanks!');
+	die ('Please do not load this page directly.');
 if ( post_password_required() ) {
 ?>
 	<p>This post is password protected. Enter the password to view comments.</p>
@@ -54,15 +54,16 @@ else : // comments are closed
     <h3><?php comment_form_title( 'Write a Comment', 'Post a reply to %s' ); ?></h3>
 
 <?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
-<p>You must be <a href="<?php echo wp_login_url( get_permalink() ); ?>">logged in</a> to post a comment.</p>
+<p>You must be <a href="<?php echo wp_login_url( get_permalink() ); ?>" class="login-link">logged in</a> to post a comment.</p>
 <?php else : ?>
 
     <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="CommentForm" name="commentform" autocomplete="on">
 
 <?php if (is_user_logged_in()) : ?>
 
-		<p>You are logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a> | <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="Log out of this account" class="icon-logout">Log out</a></p>
-		<input type="hidden" name="author" id="author" value="<?php echo $user_identity; ?>"><input type="hidden" name="email" id="email" value="xyz@xyz.com">   
+		<p>You are logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a> | <a href="<?php echo wp_logout_url(get_permalink()); ?>" class="logout-link">Log out</a></p>
+		<input type="hidden" name="author" id="author" value="<?php echo $user_identity; ?>">
+        <input type="hidden" name="email" id="email" value="<?php printf("%s@%s", $user_identity, $_SERVER['HTTP_HOST']); ?>">   
 
 <?php else : ?>
 

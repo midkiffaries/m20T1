f18632d508850ce842943c4f2be4da7e55ae2614
Ted Balmer <Ted.balmer@gmail.com>
2021-09-09 22:56:00 -0500

    09-9-2021

--- config-template.php
+++ config-template.php
@@ -15,6 +15,7 @@ $config = json_decode($ThemeSettings);
 WordPress plugins used in this theme:
 XML Sitemaps - https://wordpress.org/plugins/google-sitemap-generator/
 SVG Support - https://wordpress.org/plugins/svg-support/
+Accordion Blocks - https://wordpress.org/plugins/accordion-blocks/
 */
 
 ?>
---
+++ page-alternate.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<main class="page-main page-simple page-<?php preg_replace('/\s+/', '-', the_title()); ?>">
+    <div class="page-content width-full" role="main">
+
+<?php breadcrumb_trail(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<article <?php post_class(); ?> id="<?php printf($post->post_name); ?>" name="<?php printf($post->post_name); ?>" itemscope itemtype="http://schema.org/NewsArticle">
+    <div class="post-container">
+        <h2 class="page-title"><?php the_title(); ?></h2>
+<?php the_content("<p>Read the rest of this section &raquo;</p>"); ?>
+        
+        <p class="page-last-updated"><?php printf( __( 'Page last modified: <time itemprop="dateModified">%s</time>', 'textdomain' ), get_the_modified_date() ); ?></p>
+    </div>
+</article>
+
+<?php endwhile; endif; ?>
+
+    </div>
+
+    <!--div id="singlepage-widgets" class="page-sidebar singlepage-widgets">
+        <?php //if (is_active_sidebar( 'frontpage' )) : ?>
+        <?php //dynamic_sidebar( 'frontpage' ); ?>
+        <?php //endif; ?>
+    </div-->
+
+</main>
+
+<?php  get_footer(); ?>

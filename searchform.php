a4b350e1aebc80989c3535332f865fd91c33d7b9
Ted Balmer <Ted.balmer@gmail.com>
2022-07-19 20:35:02 -0500

    07-19-2022

--- searchform.php
+++ searchform.php
@@ -1,10 +1,10 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
 
 <?php $unique_id = wp_unique_id( 'search-form-' ); ?>
-<section class="search-block" id="<?php echo esc_attr($unique_id); ?>" aria-label="Search Form">
+<div class="search-block" id="<?php echo esc_attr($unique_id); ?>" aria-label="Search Form">
     <form method="get" class="search-form" role="search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
         <div class="search-container">
             <input type="search" class="search-input" name="s" id="<?php echo esc_attr($unique_id); ?>" value="<?php echo esc_attr(get_search_query()); ?>" placeholder="Search blog posts, pages, etc." autocapitalize="none" autocorrect="off" accesskey="s" maxlength="255" pattern="[^'\x22]+" aria-label="Search Input" required><input type="submit" value="&nbsp;" class="search-submit" aria-label="Submit Search">
         </div>
     </form>
-</section>
+</div>

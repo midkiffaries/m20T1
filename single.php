d0989f30f6285144d63820d00a81b68f85635312
Ted Balmer <Ted.balmer@gmail.com>
2022-08-06 20:52:54 -0500

    08-06-2022

--- single.php
+++ single.php
@@ -14,8 +14,8 @@
                 <h1 class="single-entry-title" id="<?php echo $post->post_name; ?>" itemprop="title"><?php the_title(); ?></h1>
                 <div class="single-entry-metadata">
                     <span class="single-entry-date"><time datetime="<?php printf(get_the_date('c')); ?>" itemprop="datePublished"><?php the_date(); ?></time></span> 
-                    <span class="single-entry-author"><span class="entry-separator"><?php echo POST_SEPARATOR; ?></span> Written By <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>" itemprop="author" rel="author"><?php the_author(); ?></a></span>
-                    <span class="single-entry-read-time"><span class="entry-separator"><?php echo POST_SEPARATOR; ?></span><?php echo reading_time(); ?></span>
+                    <span class="single-entry-author"><?php echo post_separator(); ?> Written By <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>" itemprop="author" rel="author"><?php the_author(); ?></a></span>
+                    <span class="single-entry-read-time"><?php echo post_separator(); ?> <?php echo reading_time(); ?></span>
                 </div>
             </header>
             <div class="the-content single-entry-content <?php echo ResizeFontClass($post->post_content); ?>">

5d16a5bac2f3e2cbbeaa8a7520610cd8470da454
Ted Balmer <Ted.balmer@gmail.com>
2022-08-21 13:02:59 -0500

    08-21-2022

--- functions.php
+++ functions.php
@@ -697,15 +697,15 @@ function attachment_page_image($id) {
     $image_ext = array('jpg', 'jpeg', 'png', 'gif', 'webp', 'ico', 'mp3', 'm4a', 'ogg', 'wav', 'mp4', 'm4v', 'mov', 'wmv', 'avi', 'webm', 'mpg', 'ogv', '3gp', '3g2');
     $fileExt = get_file_extension(wp_get_attachment_url($id));
 
-    // Check if attachment matches the ext array
+    // Check if attachment matches the extension array
     foreach ($image_ext as $ext) {
         if (strpos($fileExt, $ext) !== FALSE) {
-            return wp_get_attachment_image($id, 'large', 0); // Return attachment
+            return wp_get_attachment_image($id, 'large', 0, array('loading' => '')); // Return attachment
         }
     }
     // Check if attachment is SVG or other document
     if ($fileExt == 'svg' || $fileExt == 'svgz') { // SVG Images
-        return '<img src="' . wp_get_attachment_url($id) . '" alt="' . wp_get_attachment_caption($id) . '">';
+        return '<img src="' . wp_get_attachment_url($id) . '" alt="' . wp_get_attachment_caption($id) . '" class="attachment-svg">';
     } else { // All other documents types
         return '<svg id="GenericDoc" xmlns="http://www.w3.org/2000/svg" width="512" height="512"><path d="M458.9 114.5c-11.1-15.1-26.6-32.8-43.6-49.8S380.6 32.2 365.5 21C339.7 2.1 327.2 0 320 0H72C50 0 32 18 32 40v432c0 22 18 40 40 40h368c22 0 40-18 40-40V160c0-7.2-2.2-19.7-21.1-45.5zm-66.2-27.2A436.4 436.4 0 0 1 429 128h-77V51a436 436 0 0 1 40.7 36.3zM448 472c0 4.3-3.7 8-8 8H72c-4.3 0-8-3.7-8-8V40c0-4.3 3.7-8 8-8h248v112a16 16 0 0 0 16 16h112v312z" fill="dodgerblue"/><path d="M368 416H144a16 16 0 0 1 0-32h224a16 16 0 1 1 0 32zm0-64H144a16 16 0 0 1 0-32h224a16 16 0 1 1 0 32zm0-64H144a16 16 0 0 1 0-32h224a16 16 0 1 1 0 32z" fill="lightblue"/></svg>';
     }

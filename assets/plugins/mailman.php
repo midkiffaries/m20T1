44402c8902d7d2856a786dbb815491dd5efa6d01
Ted Balmer <Ted.balmer@gmail.com>
2022-09-03 20:27:05 -0500

    09-03-2022

--- assets/plugins/mailman.php
+++ assets/plugins/mailman.php
@@ -7,23 +7,26 @@ Send an email to a specified recipient
 // Includes
 include '../../functions.php';
 
+// Return and new line
+$cr = "\r\n";
+
 // Reciepent of the email
 $Recipient = bloginfo('admin_email');
 
 // Clean user input
 $Name = cleanUserInput($_GET['name']);
 $Email = strtolower(cleanUserInput($_GET['email']));
-$Message = wordwrap(cleanUserInput($_GET['message']), 70, "\r\n");
+$Message = wordwrap(cleanUserInput($_GET['message']), 70, $cr);
 
 // Email header info
 $SiteUrl = ltrim($_SERVER['HTTP_HOST'], "www.");
 $Date = date("l jS \of F Y h:i:s A");
 
 // Email Header
-$mailHeader = "From: {$SiteUrl} <website@{$SiteUrl}>" . "\r\n" .
-	"Reply-To: {$Name} <{$Email}>" . "\r\n" .
-	"Content-Type: text/plain; charset=utf-8" . "\r\n" .
-	"MIME-Version: 1.0" . "\r\n" .
+$mailHeader = "From: {$SiteUrl} <website@{$SiteUrl}>" . $cr .
+	"Reply-To: {$Name} <{$Email}>" . $cr .
+	"Content-Type: text/plain; charset=utf-8" . $cr .
+	"MIME-Version: 1.0" . $cr .
 	"X-Mailer: PHP/" . phpversion();
 
 // The Subject

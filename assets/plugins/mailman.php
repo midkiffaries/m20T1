94d6013bb9f6d89ceffbca553003bccc4fc07def
Ted Balmer <Ted.balmer@gmail.com>
2022-12-14 21:54:17 -0600

    12-14-2022

--- assets/plugins/mailman.php
+++ assets/plugins/mailman.php
@@ -7,6 +7,16 @@ Send an email to a specified recipient
 // Includes
 include '../../functions.php';
 
+// alternate
+add_action( 'phpmailer_init', 'mailer_config', 10, 1);
+function mailer_config(PHPMailer $mailer){
+	$mailer->IsSMTP();
+	$mailer->Host = "mail.marchtwenty.com"; // your SMTP server
+	$mailer->Port = 25;
+	$mailer->SMTPDebug = 2; // write 0 if you don't want to see client/server communication in page
+	$mailer->CharSet  = "utf-8";
+}
+
 // Return and new line
 $cr = "\r\n";
 
@@ -38,7 +48,7 @@ $mailMessage = $Message . "\r\n\n\t" . $Date;
 // Send the email
 if ($Name && $Email && $Message) {
 	// Email Sent
-	mail($Recipient, $mailSubject, $mailMessage, $mailHeader);
+	wp_mail($Recipient, $mailSubject, $mailMessage, $mailHeader);
 	echo true;
 } else {
 	// Error

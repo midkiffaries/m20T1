5eb41e2e4477d912c9fff93903f9b534084e05c1
Ted Balmer <Ted.balmer@gmail.com>
2022-12-16 20:17:48 -0600

    12-16-2022

--- assets/plugins/mailman.php
+++ assets/plugins/mailman.php
@@ -7,16 +7,6 @@ Send an email to a specified recipient
 // Includes
 include '../../functions.php';
 
-// alternate
-add_action( 'phpmailer_init', 'mailer_config', 10, 1);
-function mailer_config(PHPMailer $mailer){
-	$mailer->IsSMTP();
-	$mailer->Host = "jackson.asoshared.com"; // your SMTP server
-	$mailer->Port = 25;
-	$mailer->SMTPDebug = 2; // write 0 if you don't want to see client/server communication in page
-	$mailer->CharSet  = "utf-8";
-}
-
 // Return and new line
 $cr = "\r\n";
 

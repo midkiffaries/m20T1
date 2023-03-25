1f9bbb0dfd459e417f62dbeca6c5dbfb210a7a8d
Ted Balmer <Ted.balmer@gmail.com>
2023-03-25 00:38:39 -0500

    03-25-2023

--- search.php
+++ search.php
@@ -8,7 +8,7 @@
 
     <section class="search-page-form" id="search-page">
         <div>
-            <h1 class="page-title" itemprop="name">Your search netted <b><?php printf(SearchCount($s)); ?></b> result(s)</h1>
+            <h1 class="page-title" itemprop="name">Your search netted <b><?=SearchCount($s); ?></b> result(s)</h1>
             <?php get_search_form('search'); // Search form ?>
         </div>
     </section>
@@ -20,16 +20,16 @@
         <div class="post-<?php the_ID(); ?> post type-post status-publish format-standard hentry" id="post-<?php the_ID(); ?>">
             <div class="post-container">
                 <header class="entry-header">
-                    <h2 class="entry-title" id="<?php echo $post->post_name; ?>" itemprop="name"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+                    <h2 class="entry-title" id="<?=$post->post_name; ?>" itemprop="name"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
                     <div class="entry-info">
-                        <span class="entry-type"><?php echo get_post_type(); ?></span>
-                        <span class="entry-date"><time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished"><?php echo get_the_date(); ?></time></span>
-                        <span class="entry-author"><?php echo post_separator(); ?> By <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>" itemprop="author" rel="author"><?php the_author(); ?></a></span>
+                        <span class="entry-type"><?=get_post_type(); ?></span>
+                        <span class="entry-date"><time datetime="<?=get_the_date('c'); ?>" itemprop="datePublished"><?=get_the_date(); ?></time></span>
+                        <span class="entry-author"><?=post_separator(); ?> By <a href="<?=get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>" itemprop="author" rel="author"><?php the_author(); ?></a></span>
                     </div>
                 </header>
                 <div class="the-content content-excerpt" itemprop="description">
-                    <img class="entry-square-thumbnail" src="<?php echo FeaturedImageURL(get_the_ID(), 'thumbnail', false); ?>" alt="">
-                    <p><?php echo shorten_the_content($post->post_content); ?></p>
+                    <img class="entry-square-thumbnail" src="<?=FeaturedImageURL(get_the_ID(), 'thumbnail', false); ?>" alt="">
+                    <p><?=shorten_the_content($post->post_content); ?></p>
                     <p class="entry-readmore"><a href="<?php the_permalink(); ?>" rel="bookmark">Continue reading...</a></p>
                 </div>
                 <footer class="entry-footer hidden">
@@ -52,17 +52,17 @@
 
     <section class="search-page-form" id="search-page">
         <div>
-            <h1 class="page-title" itemprop="title">Your search netted <strong><?php printf(SearchCount($s)); ?></strong> result(s)</h1>
+            <h1 class="page-title" itemprop="title">Your search netted <strong><?=SearchCount($s); ?></strong> result(s)</h1>
             <?php get_search_form('search'); // Search form ?>
         </div>
     </section>
 
     <article class="search-results" itemscope itemtype="http://schema.org/NewsArticle">
         <div class="the-content aligncenter">
-            <p>The search query of <strong><?php echo esc_attr(get_search_query()); ?></strong> came up empty.</p>
+            <p>The search query of <strong><?=esc_attr(get_search_query()); ?></strong> came up empty.</p>
             <p>If it will make you feel better, this probably happens to Google too.</p>
             <div class="page-search-image">
-                <?php echo SEARCH_ERROR_IMAGE; ?>
+                <?=SEARCH_ERROR_IMAGE; ?>
             </div>
         </div>
     </article>

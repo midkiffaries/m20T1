745ce671bf0be8f01d46a739acfd79d40143e58b
Ted Balmer <Ted.balmer@gmail.com>
2023-05-03 19:43:53 -0500

    05-03-2023

--- searchform.php
+++ searchform.php
@@ -6,7 +6,7 @@
     <form method="get" class="search-form" role="search" action="<?=esc_url( home_url( '/' ) ); ?>" itemscope itemtype="https://schema.org/SearchAction" itemprop="potentialAction">
         <h3 class="search-title" itemprop="name">Search <?=bloginfo('name'); ?></h3>
         <div class="search-container">
-            <input type="hidden" itemprop="target" content="<?=esc_url( home_url( '/' ) ); ?>?q={query}">
+            <input type="hidden" itemprop="target" content="<?=esc_url( home_url( '/' ) ); ?>?q=<?=esc_attr(get_search_query()); ?>">
             <input type="search" class="search-input" name="s" id="<?=esc_attr($unique_id); ?>" value="<?=esc_attr(get_search_query()); ?>" placeholder="Search blog posts, pages, etc." autocapitalize="none" autocorrect="off" accesskey="s" maxlength="255" pattern="[^'\x22]+" aria-label="Search Input" required><input type="submit" value="&nbsp;" itemprop="query" class="search-submit" aria-label="Submit Search">
         </div>
     </form>

30608ed82c5c055a7ced9a78931f2c655f8c7151
Ted Balmer <Ted.balmer@gmail.com>
2023-06-24 10:33:57 -0500

    06-24-2023

--- single-portfolio.php
+++ single-portfolio.php
@@ -8,6 +8,7 @@
 
         <article <?php post_class(); ?> id="<?=$post->post_name; ?>" name="<?=$post->post_name; ?>" itemscope itemtype="https://schema.org/CreativeWork">
             <div class="post-container">
+                <div class="portfolio-name" itemprop="articleSection"><?=get_post_type(); ?></div>
                 <h1 class="page-title" itemprop="name headline"><?php the_title(); ?></h1>
                 <div class="the-content" itemprop="text description">
                     <?php the_content("<p>Continue Reading &raquo;</p>"); ?>

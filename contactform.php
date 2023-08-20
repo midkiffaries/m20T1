ec786530b63b80c9c88bba38fbd4eb67cf48f1f6
Ted Balmer <Ted.balmer@gmail.com>
2023-08-20 11:21:28 -0500

    08-20-2023

--- contactform.php
+++ contactform.php
@@ -1,6 +1,6 @@
 <?php // Global Contact Form ?>
 <?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
-<div class="email-block">
-	<h3>Send me a message</h3>
+<div class="email-block" aria-label="Contact Form">
+	<h3 itemprop="name">Send me a message</h3>
 	<?=apply_shortcodes( '[contact-form-7 id="3653" title="Main Contact Form"]' ); ?>
 </div>

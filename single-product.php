3a2ad648a9b50d627465e980c104acb85cbb797a
Ted Balmer <Ted.balmer@gmail.com>
2023-11-29 16:13:21 -0600

    11-29-2023

--- single-product.php
+++ single-product.php
@@ -9,11 +9,10 @@
 
         <article <?php post_class(); ?> id="<?=$post->post_name; ?>" name="<?=$post->post_name; ?>" itemscope itemtype="https://schema.org/Product">
             <div class="post-container">
-                <div class="product-title" itemprop="articleSection">Product</div>
+                <div class="product-title" itemprop="articleSection"><a href="<?=home_url() . "/" . get_post_type() . "/"; ?>">Shop</a></div>
                 <h1 class="page-title" itemprop="name headline"><?php the_title(); ?></h1>
                 <div class="the-content" itemprop="text description">
                     <?php the_content("<p>Continue Reading &raquo;</p>"); ?>
-                    <?=custom_page_css(get_the_ID()); ?>
                 </div>
             </div>
         </article>
@@ -25,11 +24,8 @@
     <?php endwhile; endif; ?>
 
         <aside id="page-widgets" class="page-sidebar product-widgets width-full">
-            <?php dynamic_sidebar( selectSidebarCustomField(get_the_ID(), 'singlepost') ); // Select from 'Widgets_Slug' custom field ?>
-        
-            <div class="widget child-page-widget">
-                <?php //get_child_pages(get_the_ID(), false); // Display the children of this page ?>
-            </div>
+            <?php dynamic_sidebar( selectSidebarCustomField(get_the_ID(), 'singlepage') ); // Select from 'Widgets_Slug' custom field ?>
+
         </aside>
 
     </div>

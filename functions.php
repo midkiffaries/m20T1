be73147f9bb215684d34dde1d8252879b908ff67
Ted Balmer <Ted.balmer@gmail.com>
2024-05-07 22:40:24 -0500

    05-07-2024

--- functions.php
+++ functions.php
@@ -1307,11 +1307,11 @@ function m20T1_settings_page() {
                 <td><textarea name="404_text" id="404_text" class="code" placeholder="That page must have been deleted or is otherwise inaccessable." rows="3" spellcheck="false" autocapitalize="none" autocorrect="off"><?=esc_attr(clean_html(get_option('404_text')));?></textarea><small>Allowed HTML tags: <b>&lt;b&gt; &lt;strong&gt; &lt;i&gt; &lt;em&gt; &lt;a&gt; &lt;span&gt; &lt;abbr&gt;</b></small></td>
             </tr>
             <tr valign="top">
-                <th scope="row"><label for="404_image">404 Error Page Image URL</label></th>
+                <th scope="row"><label for="404_image">404 Error Page Image URL</label> <br><small>500x500px</small></th>
                 <td><input type="url" name="404_image" id="404_image" placeholder="<?=esc_url(home_url() . "/wp-content/uploads/FILENAME.JPG");?>" spellcheck="false" autocapitalize="none" autocorrect="off" inputmode="url" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" value="<?=esc_attr(get_option('404_image'));?>"> <img src="<?=esc_attr(get_option('404_image'));?>" alt="" class="form-image" loading="lazy" decoding="async"></td>
             </tr>
             <tr valign="top">
-                <th scope="row"><label for="search_image">Search Page Image URL</label></th>
+                <th scope="row"><label for="search_image">Search Page Image URL</label> <br><small>500x500px</small></th>
                 <td><input type="url" name="search_image" id="search_image" placeholder="<?=esc_url(home_url() . "/wp-content/uploads/FILENAME.JPG");?>" spellcheck="false" autocapitalize="none" autocorrect="off" inputmode="url" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" value="<?=esc_attr(get_option('search_image'));?>"> <img src="<?=esc_attr(get_option('search_image'));?>" alt="" class="form-image" loading="lazy" decoding="async"></td>
             </tr>
         </table>
@@ -1319,11 +1319,11 @@ function m20T1_settings_page() {
         <p>Set the fallback image for the social media sharing metadata and featured image.</p>
         <table class="form-table" role="presentation">
             <tr valign="top">
-                <th scope="row"><label for="social_image">Social Image Fallback URL</label></th>
+                <th scope="row"><label for="social_image">Social Image Fallback URL</label> <br><small>1000x500px</small></th>
                 <td><input type="url" name="social_image" id="social_image" placeholder="<?=esc_url(home_url() . "/wp-content/uploads/FILENAME.JPG");?>" spellcheck="false" autocapitalize="none" autocorrect="off" inputmode="url" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" value="<?=esc_attr(get_option('social_image'));?>"> <img src="<?=esc_attr(get_option('social_image'));?>" alt="" class="form-image" loading="lazy" decoding="async"></td>
             </tr>
             <tr valign="top">
-                <th scope="row"><label for="blank_image">Featured Image Fallback URL</label></th>
+                <th scope="row"><label for="blank_image">Featured Image Fallback URL</label> <br><small>600x600px</small></th>
                 <td><input type="url" name="blank_image" id="blank_image" placeholder="<?=esc_url(home_url() . "/wp-content/uploads/FILENAME.JPG");?>" spellcheck="false" autocapitalize="none" autocorrect="off" inputmode="url" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" value="<?=esc_attr(get_option('blank_image'));?>"> <img src="<?=esc_attr(get_option('blank_image'));?>" alt="" class="form-image" loading="lazy" decoding="async"></td>
             </tr>
         </table>

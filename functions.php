8db4c7ec3d334f26a14013bb7c25f8a30ff576ee
Ted Balmer <Ted.balmer@gmail.com>
2024-05-10 16:55:16 -0500

    05-10-2024

--- functions.php
+++ functions.php
@@ -1310,11 +1310,11 @@ function m20T1_settings_page() {
             </tr>
             <tr valign="top">
                 <th scope="row"><label for="404_image">404 Error Page Image URL</label> <br><small>500x500px <i>Type: all</i></small></th>
-                <td><input type="url" name="404_image" id="404_image" placeholder="<?=esc_url(home_url() . "/wp-content/uploads/FILENAME.JPG");?>" spellcheck="false" autocapitalize="none" autocorrect="off" inputmode="url" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" value="<?=esc_attr(get_option('404_image'));?>"> <img src="<?=esc_attr(get_option('404_image'));?>" alt="" class="form-image" loading="lazy" decoding="async"></td>
+                <td><input type="url" name="404_image" id="404_image" placeholder="<?=esc_url(home_url() . "/wp-content/uploads/filename.jpg");?>" spellcheck="false" autocapitalize="none" autocorrect="off" inputmode="url" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" value="<?=esc_attr(get_option('404_image'));?>"> <img src="<?=esc_attr(get_option('404_image'));?>" alt="" class="form-image" loading="lazy" decoding="async"></td>
             </tr>
             <tr valign="top">
                 <th scope="row"><label for="search_image">Search Page Image URL</label> <br><small>500x500px <i>Type: all</i></small></th>
-                <td><input type="url" name="search_image" id="search_image" placeholder="<?=esc_url(home_url() . "/wp-content/uploads/FILENAME.JPG");?>" spellcheck="false" autocapitalize="none" autocorrect="off" inputmode="url" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" value="<?=esc_attr(get_option('search_image'));?>"> <img src="<?=esc_attr(get_option('search_image'));?>" alt="" class="form-image" loading="lazy" decoding="async"></td>
+                <td><input type="url" name="search_image" id="search_image" placeholder="<?=esc_url(home_url() . "/wp-content/uploads/filename.jpg");?>" spellcheck="false" autocapitalize="none" autocorrect="off" inputmode="url" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" value="<?=esc_attr(get_option('search_image'));?>"> <img src="<?=esc_attr(get_option('search_image'));?>" alt="" class="form-image" loading="lazy" decoding="async"></td>
             </tr>
         </table>
         <h2>Placeholder and Fallback Images</h2>

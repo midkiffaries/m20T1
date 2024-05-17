43cb67580c7f9bf58cfd3d9f37002a7607832fae
Ted Balmer <Ted.balmer@gmail.com>
2024-05-17 16:52:08 -0500

    05-17-2024

--- functions.php
+++ functions.php
@@ -555,7 +555,7 @@ function get_child_pages( $id, $thumbnail ) {
 // [list-posts posts="5" post_type="portfolio" order="asc" orderby="title" thumbnail="1" excerpt="1" category="" id="" class=""]
 add_shortcode('list-posts', function( $atts, $content = null ){
     extract(shortcode_atts(['posts' => 1, 'post_type' => 'portfolio', 'order' => 'desc', 'orderby' => 'title', 'thumbnail' => 0, 'excerpt' => 0, 'category' => '', 'id' => '', 'class' => ''], $atts));
-    query_posts(['orderby' => esc_html( $atts['orderby'] ), 'order' => esc_html( $atts['order'] ), 'post_type' => esc_html( $atts['post_type'] ), 'category' => esc_html( $atts['category'] ), 'showposts' => $posts]);
+    query_posts(['orderby' => esc_html( $atts['orderby'] ), 'order' => esc_html( $atts['order'] ), 'post_type' => esc_html( $atts['post_type'] ), 'category_name' => esc_html( $atts['category'] ), 'showposts' => $posts]);
 
     if (have_posts()) { 
         while (have_posts()) : the_post(); // List each item

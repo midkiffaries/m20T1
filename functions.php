bcda8a81dc8f07f1e2075cbd2225aafb7113b80a
Ted Balmer <Ted.balmer@gmail.com>
2024-07-07 14:05:14 -0500

    07-07-2024

--- functions.php
+++ functions.php
@@ -998,11 +998,10 @@ add_action('wp_dashboard_setup', function(){
     wp_add_dashboard_widget('custom_dashboard_text', 'm20T1 Theme Guide', 'custom_dashboard_text');
     function custom_dashboard_text() {
     ?>
-    <p>Classes: <code>alignjustify</code>, <code>subtitle</code>, <code>hidden</code></p>
-    <!--p>Additional <a href="/wp-admin/themes.php?page=<?php //get_filepath();?>/wp-content/themes/m20T1/functions.php">custom theme settings</a> are under <em>Appearance</em>.</p-->
-    <p>Additional custom theme settings are under <em>Appearance</em>.</p>
-    <h3>Post List Shortcode Sample</h3>
-    <p></p><code>[list-posts posts="5" post_type="portfolio" order="asc" orderby="title" thumbnail="1" excerpt="1" post_status="publish" category="" id="" class=""]</code></p>
+    <p><a href="<?=esc_url(home_url() . "/wp-admin/themes.php?page=") . get_filepath(esc_url(home_url() . '/wp-content/themes/m20T1/functions.php'));?>">Additional Custom Theme Settings</a></p>
+    <p>Editor Classes: <code>alignjustify</code>, <code>subtitle</code>, <code>hidden</code></p>
+    <h3>List Posts & Pages Shortcode Sample</h3>
+    <p><code style="display:block">[list-posts posts="5" post_type="portfolio" order="asc" orderby="title" thumbnail="1" excerpt="1" post_status="publish" category="" id="" class=""]</code></p>
     <?php
     }
 });

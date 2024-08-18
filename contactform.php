ec050bc23e10f3cc85fc56024866db5cd246abb5
Ted Balmer <Ted.balmer@gmail.com>
2024-08-18 18:55:05 -0500

    08-18-2024

--- contactform.php
+++ contactform.php
@@ -3,27 +3,31 @@
 <div class="email-block" aria-label="Contact Form">
 	
 	<?php if (get_option('contact_shortcode')) : ?>
-	<?=apply_shortcodes(wp_strip_all_tags(get_option('contact_shortcode')));?>
+		<?=apply_shortcodes(wp_strip_all_tags(get_option('contact_shortcode')));?>
 	<?php else : ?>
-	<?=comment_form([
-		'label_submit' => __( 'Send Message', 'm20t1' ),
-		'title_reply' => __( '<h3>Send me a message</h3>', 'm20t1' ),
-		'comment_notes_before' => '',
-		'comment_notes_after' => '',
-		'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Message', 'noun' ) . '<span class="required">*</span></label><textarea id="comment" name="comment" aria-required="true" placeholder="What would you like to tell me..." required></textarea></p>',
-	], 5 ); // post id number / open comments on post ?>
-	<style>
-		.email-block .comment-form {
-			background: none;
-			padding: 0;
-		}
-		.email-block .comment-form-url {
-			display: none;
-		}
-		.email-block .required {
-			color: #0000;
-		}
-	</style>
+		<?php if (comments_open(get_option('page_on_front'))) : ?>
+		<?=comment_form([
+			'label_submit' => __( 'Send Message', 'm20t1' ),
+			'title_reply' => __( '<h3>Send me a message</h3>', 'm20t1' ),
+			'comment_notes_before' => '',
+			'comment_notes_after' => '',
+			'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Message', 'noun' ) . '<span class="required">*</span></label><textarea id="comment" name="comment" aria-required="true" placeholder="What would you like to tell me..." required></textarea></p>',
+		], get_option('page_on_front') ); // ID of the site's homepage ?>
+		<style>
+			.email-block .comment-form {
+				background: none;
+				padding: 0;
+			}
+			.email-block .comment-form-url {
+				display: none;
+			}
+			.email-block .required {
+				color: #0000;
+			}
+		</style>
+		<?php else : ?>
+			<p>Discussion <i>closed</i> on the Front Page.</p>
+		<?php endif; ?>
 	<?php endif; ?>
 	
 </div>

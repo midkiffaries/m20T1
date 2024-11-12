42b10a74a13c817db74a1ecee9206a8d5a3e6dfa
Ted Balmer <Ted.balmer@gmail.com>
2024-11-11 19:28:21 -0600

    11-11-2024

--- functions.php
+++ functions.php
@@ -649,6 +649,7 @@ add_action( 'init', function(){
     remove_action( 'wp_head', 'wp_site_icon', 99 );
 
     // Remove WordPress Emojis
+    remove_action( 'wp_head', 'rel_canonical' );
     remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
     remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
     remove_action( 'wp_print_styles', 'print_emoji_styles' );
@@ -747,6 +748,7 @@ add_action( 'wp_head', function(){
 <meta name="title" content="<?=bloginfo('name');?>">
 <meta name="generator" content="m20T1 WordPress Theme by Ted Balmer">
 <meta name="author" content="<?=get_the_author_meta('display_name', get_post_field ('post_author', get_the_ID()));?>">
+<link rel="canonical" href="<?=the_permalink();?>">
 <link rel="dns-prefetch" href="<?=esc_url(preg_replace("(^https?:)", '', home_url()));?>">
 <link rel="pingback" href="<?=bloginfo('pingback_url');?>">
 <link rel="Siteuri" href="<?=home_url();?>/" id="SiteURI">

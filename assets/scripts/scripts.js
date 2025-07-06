1ef7af46821b6785abad082b20c3ab638335d77b
Ted Balmer <Ted.balmer@gmail.com>
2025-07-06 12:10:19 -0500

    7-06-2025

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -844,3 +844,17 @@ function closeModals(c) {
 function isLinkAnImage(v) {
     return ['jpg','jpeg','jp2','png','webp','gif','heif','avif'].includes(v.split('.').pop()) ? true : false;
 }
+
+// Converts an integer to roman numerals
+function toRoman(num) {
+    const numerals = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+    let result = '';
+    
+    for (const key in numerals) {
+        while (num >= numerals[key]) {
+            result += key;
+            num -= numerals[key];
+        }
+    }
+    return result;
+}

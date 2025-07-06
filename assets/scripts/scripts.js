321325b32c5107518570501531871e165a35411f
Ted Balmer <Ted.balmer@gmail.com>
2025-07-06 12:11:02 -0500

    7-06-2025

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -849,7 +849,6 @@ function isLinkAnImage(v) {
 function toRoman(num) {
     const numerals = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
     let result = '';
-    
     for (const key in numerals) {
         while (num >= numerals[key]) {
             result += key;

0fabf9e332de4ba160bd5351ecf12fd9fd62ec35
Ted Balmer <Ted.balmer@gmail.com>
2025-12-26 15:27:24 -0600

    12-26-2025

--- functions.php
+++ functions.php
@@ -286,12 +286,6 @@ add_action( 'init', function(){
         'name'  => 'button-glass',
         'label' => __( 'Glass', 'm20t1' )
     ]);
-    // Accordion: Left Cross style
-    register_block_style( 'core/accordion-item', [
-        'name'  => 'accordion-cross',
-        'label' => __( 'Cross', 'm20t1' ),
-        'inline_style' => '.dark-mode .is-style-accordion-cross .wp-block-accordion-heading::before{filter:invert()} .is-style-accordion-cross .wp-block-accordion-heading::-webkit-accordion-marker, .is-style-accordion-cross .wp-block-accordion-heading::marker {display:none;content:""}'
-    ]);
     // Accordion: Right Arrow style
     register_block_style( 'core/accordion-item', [
         'name'  => 'accordion-arrow',
